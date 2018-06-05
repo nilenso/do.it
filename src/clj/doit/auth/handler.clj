ce12dda67eea15c6d4575bf93ac1a5d45dc49663
Harsh Gupta <harsh@nilenso.com>
2018-06-05 15:14:50 +0530

    Expose google-client-id through api endpoint

--- src/clj/doit/auth/handler.clj
+++ src/clj/doit/auth/handler.clj
@@ -1,7 +1,15 @@
 (ns doit.auth.handler
   (:require
    [clojure.data.json :as json]
-   [doit.config :as config]))
+   [doit.auth.auth :as auth]
+   [doit.config :as config]
+   [org.httpkit.client :as http]
+   [doit.user.db :as user-db]))
+
+(defn wrap-response [data status]
+  {:status status
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str data)})
 
 (defn client-id [request]
   {:status 200
