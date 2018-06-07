6fd8bef5faf165246dfb2c085b182b521939a7fb
Harsh Gupta <harsh@nilenso.com>
2018-06-07 13:05:24 +0530

    Make email and created_at fields in app_user non null

--- resources/migrations/004-add-table-user.up.sql
+++ resources/migrations/004-add-table-user.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE app_user (
        id SERIAL PRIMARY KEY,
         -- email max length reference https://stackoverflow.com/a/574698
-       email VARCHAR(255),
+       email VARCHAR(255) NOT NULL,
        token TEXT,
        token_exp INTEGER,
-       created_at timestamptz DEFAULT now()
+       created_at timestamptz DEFAULT now() NOT NULL
 );
