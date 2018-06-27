a801d6a4f0d75deef7bac332046554b16d60185a
Harsh Gupta <harsh@nilenso.com>
2018-06-27 17:54:28 +0530

    Return 404 for non existant api endpoints

--- src/clj/doit/route.clj
+++ src/clj/doit/route.clj
@@ -4,6 +4,7 @@
             [doit.todo-list.route :as todo-list-route]
             [doit.middleware :as mw]))
 
-(def routes ["/api/" {"todo/"      todo-route/routes
-                      "auth/"      auth-route/routes
-                      "todo-list/" todo-list-route/routes}])
+(def routes ["/" {"api/" {"todo/"      todo-route/routes
+                          "auth/"      auth-route/routes
+                          "todo-list/" todo-list-route/routes}
+                  true   (fn [request] {:status 404 :body {:error "Not Found"}})}])
